8fa021019147b13f75cee575c2c72b9f4c58340c
Cavan Vannice <CavanVannice@outlook.com>
2024-02-20 09:35:47 -0500

    Added dynamic categories

--- src/App.js
+++ src/App.js
@@ -1,4 +1,38 @@
 const App = () => {
-  return <div>Hello World!</div>;
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats'
+    },
+    {
+      id: 1,
+      title: 'Jackets'
+    },
+    {
+      id: 1,
+      title: 'Sneakers'
+    },
+    {
+      id: 1,
+      title: 'Womens'
+    },
+    {
+      id: 1,
+      title: 'Mens'
+    },
+  ]
+  return (
+    <div className="categories-container">
+      {categories.map(({title}) => (
+        <div className="category-container">
+          <div className="background-image"></div>
+          <div className="category=body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+            </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 export default App;
