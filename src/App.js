72d76bb854c6320a564cbaa27c99c0a3becea887
Cavan Vannice <CavanVannice@outlook.com>
2024-02-20 09:53:53 -0500

    Added sass and some styles

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './categories.styles.scss';
 const App = () => {
   const categories = [
     {
@@ -5,31 +6,31 @@ const App = () => {
       title: 'Hats'
     },
     {
-      id: 1,
+      id: 2,
       title: 'Jackets'
     },
     {
-      id: 1,
+      id: 3,
       title: 'Sneakers'
     },
     {
-      id: 1,
+      id: 4,
       title: 'Womens'
     },
     {
-      id: 1,
+      id: 5,
       title: 'Mens'
-    },
+    }
   ]
   return (
     <div className="categories-container">
-      {categories.map(({title}) => (
-        <div className="category-container">
+      {categories.map(({title, id}) => (
+        <div key={id} className="category-container">
           <div className="background-image"></div>
-          <div className="category=body-container">
+          <div className="category-body-container">
             <h2>{title}</h2>
             <p>Shop Now</p>
-            </div>
+          </div>
         </div>
       ))}
     </div>
