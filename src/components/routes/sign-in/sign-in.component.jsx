a46e1f2738291d2a6d48497902e7f30527f4e1f6
Cavan Vannice <CavanVannice@outlook.com>
2024-03-01 15:03:17 -0500

    Add Google sign-in functionality and SignUpForm component

--- src/components/routes/sign-in/sign-in.component.jsx
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,26 @@
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
 import {
+	auth,
 	signInWithGooglePopup,
+	signInWithGoogleRedirect,
 	createUserDocumentFromAuth,
 } from "../../../utils/firebase/firebase.utils";
+
+import SignUpForm from "../../sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
 	const logGoogleUser = async () => {
 		const { user } = await signInWithGooglePopup();
 		const userDocRef = await createUserDocumentFromAuth(user);
 	};
+
 	return (
 		<div>
 			<h1>Sign In Page</h1>
 			<button onClick={logGoogleUser}>Signin with Google Popup</button>
+			<SignUpForm />
 		</div>
 	);
 };
